a965694107584f209ef6100b1ae940efa8f16183
Shreyash Mishra <shreyashm1601@gmail.com>
2021-11-07 12:35:35 +0530

    resolve food csv error

--- project.R
+++ project.R
@@ -1,4 +1,4 @@
-setwd("/Users/shreyash/Documents/UG3/sem5/ida/R/project/")
+setwd("/Users/shreyash/Documents/UG3/sem5/ida/R/IDA/")
 # set working directory based on your installation of R
 
 drinks <- read.csv("datasets/starbucks-menu-nutrition-drinks.csv")
@@ -12,12 +12,11 @@ ncol(drinks_exp)
 colnames(drinks_exp)
 drinks_exp[sample(nrow(drinks_exp), 3), ]
 
-#unable to load this, some problem with file it seems
-# visit: https://www.kaggle.com/starbucks/starbucks-menu/discussion/36939
-food <- read.csv("datasets/starbucks-menu-nutrition-food.csv")
+
+food <- read.csv("datasets/starbucks-menu-nutrition-food.csv",fileEncoding = "UTF-16")
 food$Carb...g.
-ncol(drinks)
-colnames(drinks)
-drinks[sample(nrow(drinks), 3), 5]
+ncol(food)
+colnames(food)
+drinks[sample(nrow(food), 3), ]
 
 
