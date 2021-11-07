6d54bcfcfe6b339d6d6611f00fdf1f6db194a1f0
Siddharth <siddharth25pandey@gmail.com>
2021-11-07 21:05:57 +0530

    Update

--- project.R
+++ project.R
@@ -6,6 +6,25 @@ setwd("C:/Users/91843/Desktop/IDA/datasets/")
 library(tidyverse) 
 library(janitor)
 library(ggplot2)
+
+#------------------StarBuck Menu Nutrition Food ----------------------------------
+
+
+food <- read.csv("starbucks-menu-nutrition-food.csv",fileEncoding = "UTF-16")
+summary(food)
+head(food,6)
+names(food)
+drinks[sample(nrow(food), 3), 5]
+colA<-food$Calories
+colB<-food$Carb...g.
+cor.test(colA,colB)
+#cor.test(fcolA,colB,alternative="less")
+#cor.test(colA,colB,alternative="greater")
+#cor.test(colA,colB,alternative="two.sided")
+ggplot(data=food, aes(x=colA, y=colB, group=1)) +
+  geom_line(color="blue",linetype="dashed")+
+  geom_point()+ggtitle("Relation Between Calories of Food and Carbs..")
+
 #------------------StarBuck Menu Nutrition Drinks ----------------------------------
 
 #drinks <- read.csv("datasets/starbucks-menu-nutrition-drinks.csv")
@@ -15,10 +34,20 @@ head(drinks,6)
 names(drinks)
 drinks$Carb...g.
 ncol(drinks)
-colnames(drinks)
 drinks[sample(nrow(drinks), 3), ]
-drinks_exp[sample(nrow(drinks_exp), 3), ]
-
+is.numeric(colC)
+class(colC)
+typeof(colC)
+typeof(colD)
+length(colC)
+length(colD)
+drinks$Calories <- as.numeric(drinks$Calories)
+drinks$Carb...g. <- as.numeric(drinks$Carb...g.)
+colC<-drinks$Calories
+colD<-drinks$Carb...g.
+cor.test(colC,colD)
+typeof(colC)
+typeof(colD)
 
 #------------------StarBuck Drinks Menu----------------------------------
 
@@ -26,23 +55,11 @@ drinks_exp <- read.csv("starbucks_drinkMenu_expanded.csv")
 summary(drinks_exp)
 head(drinks_exp,6)
 names(drinks_exp)
-ncol(drinks_exp)
-colnames(drinks_exp)
 drinks_exp[sample(nrow(drinks_exp), 3), ]
 ggplot(drinks_exp, aes(x = Sodium..mg.)) +
   geom_histogram(bins=30) +
   ggtitle("Sodium Content: Starbucks Menu")
 
-#------------------StarBuck Menu Nutrition Food ----------------------------------
 
-
-food <- read.csv("starbucks-menu-nutrition-food.csv",fileEncoding = "UTF-16")
-summary(food)
-head(food,6)
-names(food)
-food$Carb...g.
-ncol(food)
-colnames(food)
-drinks[sample(nrow(food), 3), 5]
 
 
