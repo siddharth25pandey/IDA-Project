0f07f84f9f2852a06b39e9ac9afaa4bf0144499a
Shreyash Mishra <shreyashm1601@gmail.com>
2021-11-11 15:27:54 +0530

    correlation function created

--- project.R
+++ project.R
@@ -1,23 +1,58 @@
 cat("\014") # To clear the console
-#setwd("/Users/shreyash/Documents/UG3/sem5/ida/R/project/")
-setwd("C:/Users/91843/Desktop/IDA/datasets/")
+setwd("/Users/shreyash/Documents/UG3/sem5/ida/R/project/")
+#setwd("C:/Users/91843/Desktop/IDA/datasets/")
 # set working directory based on your installation of R
 
 library(tidyverse) 
 library(janitor)
 library(ggplot2)
 
+variance <- function(X){
+  n <- length(X)
+  mX <- sum(X)/n
+  tot <- 0.0
+  for (i in X){
+    tot <- tot + (i-mX)^2
+  }
+  var <- tot/(n-1)
+  return(var)
+}
+
+pearson_corr <- function(X,Y) {
+  if (length(X) != length(Y)){
+    return("unequal columns")
+  }
+  n <- length(X)
+  mX <- sum(X)/n
+  mY <- sum(Y)/n
+  vX <- variance(X)
+  vY <- variance(Y)
+  ssX <- (vX*(n-1))^0.5
+  ssY <- (vY*(n-1))^0.5
+
+  tot <- 0.0
+  for (i in 1:length(X)){
+    tot <- tot + (X[i]-mX)*(Y[i]-mY)
+  }
+
+  r <- tot/(ssX*ssY)
+  return(r)
+}
+
+
 #------------------StarBuck Menu Nutrition Food ----------------------------------
 
 
-food <- read.csv("starbucks-menu-nutrition-food.csv",fileEncoding = "UTF-16")
+food <- read.csv("datasets/starbucks-menu-nutrition-food.csv",fileEncoding = "UTF-16")
 summary(food)
 head(food,6)
 names(food)
-drinks[sample(nrow(food), 3), 5]
+food[sample(nrow(food), 3), 5]
 colA<-food$Calories
 colB<-food$Carb...g.
 cor.test(colA,colB)
+r<-pearson_corr(colA,colB)
+print(r)
 #cor.test(fcolA,colB,alternative="less")
 #cor.test(colA,colB,alternative="greater")
 #cor.test(colA,colB,alternative="two.sided")
@@ -27,15 +62,18 @@ ggplot(data=food, aes(x=colA, y=colB, group=1)) +
 
 #------------------StarBuck Menu Nutrition Drinks ----------------------------------
 
-#drinks <- read.csv("datasets/starbucks-menu-nutrition-drinks.csv")
-drinks <- read.csv("starbucks-menu-nutrition-drinks.csv")
+drinks <- read.csv("datasets/starbucks-menu-nutrition-drinks.csv")
+#drinks <- read.csv("starbucks-menu-nutrition-drinks.csv")
 summary(drinks)
 head(drinks,6)
 names(drinks)
 drinks$Carb...g.
 ncol(drinks)
 drinks[sample(nrow(drinks), 3), ]
+colC<-drinks$Calories
+colD<-drinks$Carb...g.
 is.numeric(colC)
+is.numeric(colD)
 class(colC)
 typeof(colC)
 typeof(colD)
@@ -43,8 +81,6 @@ length(colC)
 length(colD)
 drinks$Calories <- as.numeric(drinks$Calories)
 drinks$Carb...g. <- as.numeric(drinks$Carb...g.)
-colC<-drinks$Calories
-colD<-drinks$Carb...g.
 cor.test(colC,colD)
 typeof(colC)
 typeof(colD)
