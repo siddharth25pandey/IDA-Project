6a795ea5f6bcfc6f0ffa0fe87d677080c1877123
SIDDHARTH PANDEY <56592079+siddharth25pandey@users.noreply.github.com>
2021-11-11 17:07:51 +0530

    Two_sample_t_test_and_t_value

--- project.R
+++ project.R
@@ -1,6 +1,6 @@
 cat("\014") # To clear the console
 #setwd("/Users/shreyash/Documents/UG3/sem5/ida/R/project/")
-setwd("C:/Users/Priyam Bajpai/Desktop/IDA/")
+setwd("C:/Users/91843/Desktop/IDA/datasets/")
 # set working directory based on your installation of R
 
 library(tidyverse) 
@@ -29,21 +29,61 @@ pearson_corr <- function(X,Y) {
   vY <- variance(Y)
   ssX <- (vX*(n-1))^0.5
   ssY <- (vY*(n-1))^0.5
-
+  
   tot <- 0.0
   for (i in 1:length(X)){
     tot <- tot + (X[i]-mX)*(Y[i]-mY)
   }
-
+  
   r <- tot/(ssX*ssY)
   return(r)
+}
+degree_of_freedom <- function(X,Y)
+{ if (length(X) != length(Y)){
+  return("unequal columns")
+  }
+  return ((length(X)-2))
+}
+
+two_sample_t_test <- function(X,Y)
+{
+  if (length(X) != length(Y)){
+    return("unequal columns")
+  }
+    n <- length(X)
+    mX <- sum(X)/n
+    mY <- sum(Y)/n
+    vX <- variance(X)
+    vY <- variance(Y)
+    ssX <- (vX*(n-1))^0.5
+    ssY <- (vY*(n-1))^0.5
+    #pooled_variance <- ((n-1)*vX+(n-1)*vY)/(2*n-2)
+    pooled_variance <- (vX+vY)/2
+    t <- (mX-mY)/(pooled_variance)
+    return (t)
+    
+  
+}
+
+t_value <-function(X,Y)
+{
+  if (length(X) != length(Y)){
+    return("unequal columns")
+  }
+  n <- length(X)
+  p <-pearson_corr(X,Y)
+  value_of_t <- p *((n-2)/(1-(p^2)))^0.5
+  return (value_of_t)
+  
 }
 
 
 #------------------StarBuck Menu Nutrition Food ----------------------------------
 
 
-food <- read.csv("datasets/starbucks-menu-nutrition-food.csv",fileEncoding = "UTF-16")
+food <- read.csv("starbucks-menu-nutrition-food.csv",fileEncoding = "UTF-16")
+lapply(food,function(x) { length(which(is.na(x)))})
+colSums(is.na(food))
 summary(food)
 head(food,6)
 names(food)
@@ -53,6 +93,12 @@ colB<-food$Carb...g.
 cor.test(colA,colB)
 r<-pearson_corr(colA,colB)
 print(r)
+df<-degree_of_freedom(colA,colB)
+print(df)
+tstt_test_statistics <- two_sample_t_test(colA,colB)
+print(tstt_test_statistics)
+value_of_t <-t_value(colA,colB)
+print(value_of_t)
 #cor.test(fcolA,colB,alternative="less")
 #cor.test(colA,colB,alternative="greater")
 #cor.test(colA,colB,alternative="two.sided")
@@ -62,30 +108,44 @@ ggplot(data=food, aes(x=colA, y=colB, group=1)) +
 
 #------------------StarBuck Menu Nutrition Drinks ----------------------------------
 
-drinks <- read.csv("datasets/starbucks-menu-nutrition-drinks.csv")
+drinks <- read.csv("starbucks-menu-nutrition-drinks.csv")
+drinks<-drinks[!(drinks$Calories=="-"),]
 summary(drinks)
 head(drinks,6)
 names(drinks)
-drinks<-drinks[!(drinks$Calories=="-"),]
-head(drinks,6)
-drinks[sample(nrow(drinks), 3), 5]
-colA<-drinks$Calories
-colA<-as.integer(colA)
-colB<-drinks$Carb...g.
-colB<-as.integer(colB)
-cor.test(colA,colB)
-r_d<-pearson_corr(colA,colB)
-print(r_d)
-#cor.test(fcolA,colB,alternative="less")
-#cor.test(colA,colB,alternative="greater")
-#cor.test(colA,colB,alternative="two.sided")
-ggplot(data=drinks, aes(x=colA, y=colB, group=1)) +
-  geom_line(color="red",linetype="dashed")+
-  geom_point()+ggtitle("Relation Between Calories of Drinks and Carbs..")
+drinks$Carb...g.
+ncol(drinks)
+drinks[sample(nrow(drinks), 3), ]
+colC<-drinks$Calories
+colD<-drinks$Carb...g.
+is.numeric(colC)
+is.numeric(colD)
+class(colC)
+typeof(colC)
+typeof(colD)
+length(colC)
+length(colD)
+drinks$Calories <- as.numeric(drinks$Calories)
+drinks$Carb...g. <- as.numeric(drinks$Carb...g.)
+
+typeof(colC)
+typeof(colD)
+cor.test(colC,colD)
+r<-pearson_corr(colC,colD)
+print(r)
+df<-degree_of_freedom(colC,colD)
+print(df)
+tstt_test_statistics <- two_sample_t_test(colC,colD)
+print(tstt_test_statistics)
+value_of_t <-t_value(colC,colD)
+print(value_of_t)
+
 
 #------------------StarBuck Drinks Menu----------------------------------
 
 drinks_exp <- read.csv("starbucks_drinkMenu_expanded.csv")
+lapply(drinks_exp,function(x) { length(which(is.na(x)))})
+colSums(is.na(drinks_ex))
 summary(drinks_exp)
 head(drinks_exp,6)
 names(drinks_exp)
@@ -93,7 +153,4 @@ drinks_exp[sample(nrow(drinks_exp), 3), ]
 ggplot(drinks_exp, aes(x = Sodium..mg.)) +
   geom_histogram(bins=30) +
   ggtitle("Sodium Content: Starbucks Menu")
-
-
-
 
